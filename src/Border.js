a1889cd702c4f02d7de6ee646007cffc0c78d5f5
Niklas von Hertzen <niklasvh@gmail.com>
2011-07-18 13:17:45 +0300

    moved border drawing to Borders.js

--- src/Border.js
+++ src/Border.js
@@ -16,3 +16,50 @@ html2canvas.prototype.getBorderData = function(el){
     return borders;
             
 }
+
+html2canvas.prototype.drawBorders = function(el,ctx, x, y, w, h){
+    
+    /*
+     *  TODO add support for different border-style's than solid   
+     */            
+    var borders = this.getBorderData(el);    
+    var _ = this;
+    
+    this.each(borders,function(borderSide,borderData){
+        if (borderData.width>0){
+            var bx = x,
+            by = y,
+            bw = w,
+            bh = h-(borders[2].width);
+            switch(borderSide){
+                case 0:
+                    // top border
+                    bh = borders[0].width;
+                    break;
+                case 1:
+                    // right border
+                    bx = x+w-(borders[1].width);
+                    bw = borders[1].width;                              
+                    break;
+                case 2:
+                    // bottom border
+                    by = (by+h)-(borders[2].width);
+                    bh = borders[2].width;
+                    break;
+                case 3:
+                    // left border
+                    bw = borders[3].width;  
+                    break;
+            }		
+                   
+            _.newRect(ctx,bx,by,bw,bh,borderData.color);	
+                
+                    
+          
+        }
+                
+    });
+    
+    return borders;
+    
+};
