97ddb6beba817889d7506655cd2045d32255d6b6
MoyuScript <i@moyu.moe>
2012-03-02 18:32:37 +0100

    clear the canvas with the documentElement background color
    see tests/background.html

--- src/Parse.js
+++ src/Parse.js
@@ -1250,7 +1250,7 @@ _html2canvas.Parse = function ( images, options ) {
     }
     
     
-    stack.backgroundColor = getCSS( body, "backgroundColor" );
+    stack.backgroundColor = getCSS( document.documentElement, "backgroundColor" );
     
     return stack;
 
