7726cd9f39d548eb62e0a887c82e70834df509c8
Niklas von Hertzen <niklasvh@gmail.com>
2012-03-03 00:52:46 +0200

    updated test.js to allow options

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,7 @@
 (function(document, window) {
     var scrStart = '<script type="text/javascript" src="', scrEnd = '"></script>';
     document.write(scrStart + '../external/jquery-1.6.2.js' + scrEnd);
-    var html2canvas = ['Core', 'Generate', 'Parse', 'Preload', 'Queue', 'Renderer', 'Util', 'renderers/Canvas', 'plugins/jquery.plugin.html2canvas'], i;
+    var html2canvas = ['Core', 'Generate', 'Parse', 'Preload', 'Queue', 'Renderer', 'Util', 'renderers/Canvas',  'plugins/jquery.plugin.html2canvas'], i;
     for (i = 0; i < html2canvas.length; ++i) {
         document.write(scrStart + '../src/' + html2canvas[i] + '.js' + scrEnd);
     }
@@ -17,7 +17,7 @@
             window.setUp();
         }
         setTimeout(function() {
-            $(document.body).html2canvas({
+            $(document.body).html2canvas((typeof h2cOptions !== "undefined") ? h2cOptions : {
                 flashcanvas: "../external/flashcanvas.min.js",
                 logging: true,
                 profile: true,
