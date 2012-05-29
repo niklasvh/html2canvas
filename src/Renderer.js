d88e72e1b6eaf5edf72832f3add65e3fc490da67
MoyuScript <i@moyu.moe>
2012-05-29 11:26:26 +1000

    Added a sublime-project file for Sublime Text 2 that contains the detected linting and standards set. Allows Sublime Text 2 to automatically follow the standards and have live linting enabled. Trimmed useless whitespace.

--- src/Renderer.js
+++ src/Renderer.js
@@ -9,7 +9,7 @@ _html2canvas.Renderer = function(parseQueue, options){
 
 
     var queue = [];
-   
+
     function sortZ(zStack){
         var subStacks = [],
         stackValues = [],
@@ -20,40 +20,40 @@ _html2canvas.Renderer = function(parseQueue, options){
         zValue,
         zLen,
         stackChild,
-        b, 
+        b,
         subStackLen;
-        
+
 
         for (s = 0, zLen = zStackChildren.length; s < zLen; s+=1){
-            
+
             stackChild = zStackChildren[s];
-            
+
             if (stackChild.children && stackChild.children.length > 0){
                 subStacks.push(stackChild);
                 stackValues.push(stackChild.zindex);
-            }else{         
+            }else{
                 queue.push(stackChild);
-            }  
-           
+            }
+
         }
-      
+
         stackValues.sort(function(a, b) {
             return a - b;
         });
-    
+
         for (i = 0, stackLen = stackValues.length; i < stackLen; i+=1){
             zValue = stackValues[i];
             for (b = 0, subStackLen = subStacks.length; b <= subStackLen; b+=1){
-                
+
                 if (subStacks[b].zindex === zValue){
                     stackChild = subStacks.splice(b, 1);
                     sortZ(stackChild[0]);
                     break;
-                  
+
                 }
-            }        
+            }
         }
-  
+
     }
 
 
--- src/plugins/jquery.plugin.html2canvas.js
+++ src/plugins/jquery.plugin.html2canvas.js
@@ -21,8 +21,8 @@
                 console.profileEnd();
             }
             $canvas.css({
-                position: 'absolute', 
-                left: 0, 
+                position: 'absolute',
+                left: 0,
                 top: 0
             }).appendTo(document.body);
             $canvas.siblings().toggle();
@@ -32,7 +32,7 @@
                 throwMessage("Canvas Render " + ($canvas.is(':visible') ? "visible" : "hidden"));
             });
             throwMessage('Screenshot created in '+ ((finishTime.getTime()-timer)) + " ms<br />",4000);
-            
+
             // test if canvas is read-able
             try {
                 $canvas[0].toDataURL();
@@ -43,7 +43,7 @@
                 }
             }
         };
-        
+
         html2obj = html2canvas(this, options);
 
         function throwMessage(msg,duration){
