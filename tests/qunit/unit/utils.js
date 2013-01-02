67ccb33dd5101494edbae5930771ec5f6e758397
Andy Edinborough <andy@edinborough.org>
2013-01-02 10:47:36 -0600

    add tests for resizeBounds

--- tests/qunit/unit/utils.js
+++ tests/qunit/unit/utils.js
@@ -14,5 +14,33 @@ $(function() {
        // text nodes differ
         QUnit.equal( _html2canvas.Util.Children(el[0]), arr, "Util.Children === jQuery.children()" ); 
     });
+
+    test('resizeBounds', function(){
+      
+      QUnit.deepEqual( 
+        _html2canvas.Util.resizeBounds(100, 100, 100, 100),
+        { width: 100, height: 100 },
+        'no resize'
+      );
+      
+      QUnit.deepEqual( 
+        _html2canvas.Util.resizeBounds(100, 100, 2, 100),
+        { width: 20, height: 100 },
+        'stretch'
+      );
+      
+      QUnit.deepEqual( 
+        _html2canvas.Util.resizeBounds(100, 100, 2, 100, 'contain'),
+        { width: 2, height: 2 },
+        'contain'
+      );
+      
+      QUnit.deepEqual( 
+        _html2canvas.Util.resizeBounds(100, 100, 2, 100, 'cover'),
+        { width: 100, height: 100 },
+        'contain'
+      );
+
+    });
     
 });
