ef7ffef87dcc10347d05d7eeb50901b626d0a285
MoyuScript <i@moyu.moe>
2013-08-06 10:34:13 +0800

    passes z-index8 non-positioned element with opactiy < 1
    The MDN article Understanding_z_index/Stacking_and_float is wrong about this

--- src/Renderer.js
+++ src/Renderer.js
@@ -48,10 +48,12 @@ _html2canvas.Renderer = function(parseQueue, options){
 
         // positioned after static
         context[zi].forEach(function(v) {
-          if (v.node.zIndex.isFloated) {
-            floated.push(v);
-          } else if (v.node.zIndex.isPositioned) {
+          if (v.node.zIndex.isPositioned || v.node.zIndex.opacity < 1) {
+            // http://www.w3.org/TR/css3-color/#transparency
+            // non-positioned element with opactiy < 1 should be stacked as if it were a positioned element with ‘z-index: 0’ and ‘opacity: 1’.
             positioned.push(v);
+          } else if (v.node.zIndex.isFloated) {
+            floated.push(v);
           } else {
             nonPositioned.push(v);
           }
