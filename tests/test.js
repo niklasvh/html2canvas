6ecd8bcead201ebbfe0d68f18bf7456256e721ba
MoyuScript <i@moyu.moe>
2014-02-17 00:04:09 +0200

    Fix image loading for cross-origin resources

--- tests/test.js
+++ tests/test.js
@@ -11,7 +11,7 @@ var h2cSelector, h2cOptions;
 
     document.write(srcStart + '/tests/assets/jquery-1.6.2.js' + scrEnd);
     document.write(srcStart + '/tests/assets/jquery.plugin.html2canvas.js' + scrEnd);
-    var html2canvas = ['log', 'nodecontainer', 'stackingcontext', 'textcontainer', 'support', 'imagecontainer', 'gradientcontainer', 'lineargradientcontainer', 'webkitgradientcontainer',
+    var html2canvas = ['log', 'nodecontainer', 'stackingcontext', 'textcontainer', 'support', 'imagecontainer', 'proxyimagecontainer', 'gradientcontainer', 'lineargradientcontainer', 'webkitgradientcontainer',
         'imageloader', 'nodeparser', 'font', 'fontmetrics', 'core', 'renderer', 'promise', 'renderers/canvas'], i;
     for (i = 0; i < html2canvas.length; ++i) {
         document.write(srcStart + '/src/' + html2canvas[i] + '.js?' + Math.random() + scrEnd);
@@ -30,7 +30,7 @@ var h2cSelector, h2cOptions;
                 logging: true,
                 profile: true,
                 proxy: "http://html2canvas.appspot.com/query",
-                useCORS: true
+                useCORS: false
             }, h2cOptions));
         }, 100);
     };
