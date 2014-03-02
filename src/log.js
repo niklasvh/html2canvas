f2e585be235c6bf2b3661d9e2e1d24281d5efa45
MoyuScript <i@moyu.moe>
2014-03-02 16:55:40 +0200

    Fix logging in IE9

--- src/log.js
+++ src/log.js
@@ -1,5 +1,5 @@
 function log() {
     if (window.html2canvas.logging && window.console && window.console.log) {
-        window.console.log.apply(window.console, [(Date.now() - window.html2canvas.start) + "ms", "html2canvas:"].concat([].slice.call(arguments, 0)));
+        Function.prototype.bind.call(window.console.log, (window.console)).apply(window.console, [(Date.now() - window.html2canvas.start) + "ms", "html2canvas:"].concat([].slice.call(arguments, 0)));
     }
 }
