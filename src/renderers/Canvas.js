8d70182f339a3e5d55886735dc3efb4eef8e5dd6
perymimon <pery.mimon@gmail.com>
2014-08-16 22:22:33 +0300

    add ability for bigger or smaller output
    add `option.zoom` for canvas rendered.
    now output can be X10 bigger or smaller then the original html view. 
    this is excellent for create file from canvas for printer work

--- src/renderers/Canvas.js
+++ src/renderers/Canvas.js
@@ -17,7 +17,7 @@ _html2canvas.Renderer.Canvas = function(options) {
   }
 
   function safeImage(item) {
-    if (safeImages.indexOf(item['arguments'][0].src) === -1) {
+    if (safeImages.indexOf(item['arguments'][0].src ) === -1) {
       testctx.drawImage(item['arguments'][0], 0, 0);
       try {
         testctx.getImageData(0, 0, 1, 1);
@@ -42,7 +42,8 @@ _html2canvas.Renderer.Canvas = function(options) {
             if (item['arguments'][0].width > 0 && item['arguments'][0].height > 0) {
               try {
                 ctx.fillStyle = ctx.createPattern(item['arguments'][0], "repeat");
-              } catch(e) {
+              }
+              catch(e) {
                 Util.log("html2canvas: Renderer: Error creating pattern", e.message);
               }
             }
@@ -65,63 +66,70 @@ _html2canvas.Renderer.Canvas = function(options) {
   }
 
   return function(parsedData, options, document, queue, _html2canvas) {
-    var ctx = canvas.getContext("2d"),
-    newCanvas,
-    bounds,
-    fstyle,
-    zStack = parsedData.stack;
+      var ctx = canvas.getContext("2d"),
+          newCanvas,
+          bounds,
+          fstyle,
+          zStack = parsedData.stack;
 
-    canvas.width = canvas.style.width =  options.width || zStack.ctx.width;
-    canvas.height = canvas.style.height = options.height || zStack.ctx.height;
+      options.zoom = options.zoom || 1;
 
-    fstyle = ctx.fillStyle;
-    ctx.fillStyle = (Util.isTransparent(parsedData.backgroundColor) && options.background !== undefined) ? options.background : parsedData.backgroundColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = fstyle;
-    queue.forEach(function(storageContext) {
-      // set common settings for canvas
-      ctx.textBaseline = "bottom";
-      ctx.save();
+      canvas.width = canvas.style.width =  (options.width || zStack.ctx.width) * options.zoom;
+      canvas.height = canvas.style.height = (options.height || zStack.ctx.height) * options.zoom ;
 
-      if (storageContext.transform.matrix) {
-        ctx.translate(storageContext.transform.origin[0], storageContext.transform.origin[1]);
-        ctx.transform.apply(ctx, storageContext.transform.matrix);
-        ctx.translate(-storageContext.transform.origin[0], -storageContext.transform.origin[1]);
+      fstyle = ctx.fillStyle;
+
+      ctx.scale(options.zoom, options.zoom);
+      ctx.fillStyle = (Util.isTransparent(zStack.backgroundColor) && options.background !== undefined) ? options.background : parsedData.backgroundColor;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = fstyle;
+
+      queue.forEach(function(storageContext) {
+          // set common settings for canvas
+          ctx.textBaseline = "bottom";
+          ctx.save();
+
+          if (storageContext.transform.matrix) {
+              ctx.translate(storageContext.transform.origin[0], storageContext.transform.origin[1]);
+              ctx.transform.apply(ctx, storageContext.transform.matrix);
+              ctx.translate(-storageContext.transform.origin[0], -storageContext.transform.origin[1]);
+          }
+
+          if (storageContext.clip){
+              ctx.beginPath();
+              ctx.rect(storageContext.clip.left, storageContext.clip.top, storageContext.clip.width, storageContext.clip.height);
+              ctx.clip();
+          }
+
+          if (storageContext.ctx.storage) {
+              storageContext.ctx.storage.forEach(function(item) {
+                  renderItem(ctx, item);
+              });
+          }
+
+          ctx.restore();
+      });
+
+      Util.log("html2canvas: Renderer: Canvas renderer done - returning canvas obj");
+
+      if (options.elements.length === 1) {
+          if (typeof options.elements[0] === "object" && options.elements[0].nodeName !== "BODY") {
+              // crop image to the bounds of selected (single) element
+              bounds = _html2canvas.Util.Bounds(options.elements[0]);
+              bounds.width *= options.zoom;
+              bounds.height *= options.zoom;
+              bounds.left *= options.zoom;
+              bounds.top *= options.zoom;
+              newCanvas = document.createElement('canvas');
+              newCanvas.width = Math.ceil(bounds.width );
+              newCanvas.height = Math.ceil(bounds.height );
+              ctx = newCanvas.getContext("2d");
+
+              ctx.drawImage(canvas, bounds.left, bounds.top, bounds.width, bounds.height, 0, 0, bounds.width , bounds.height );
+              canvas = null;
+              return newCanvas;
+          }
       }
-
-      if (storageContext.clip){
-        ctx.beginPath();
-        ctx.rect(storageContext.clip.left, storageContext.clip.top, storageContext.clip.width, storageContext.clip.height);
-        ctx.clip();
-      }
-
-      if (storageContext.ctx.storage) {
-        storageContext.ctx.storage.forEach(function(item) {
-          renderItem(ctx, item);
-        });
-      }
-
-      ctx.restore();
-    });
-
-    Util.log("html2canvas: Renderer: Canvas renderer done - returning canvas obj");
-
-    if (options.elements.length === 1) {
-      if (typeof options.elements[0] === "object" && options.elements[0].nodeName !== "BODY") {
-        // crop image to the bounds of selected (single) element
-        bounds = _html2canvas.Util.Bounds(options.elements[0]);
-        newCanvas = document.createElement('canvas');
-        newCanvas.width = Math.ceil(bounds.width);
-        newCanvas.height = Math.ceil(bounds.height);
-        ctx = newCanvas.getContext("2d");
-
-		var imgData = canvas.getContext("2d").getImageData(bounds.left, bounds.top, bounds.width, bounds.height);
-		ctx.putImageData(imgData, 0, 0);
-
-        canvas = null;
-        return newCanvas;
-      }
-    }
 
     return canvas;
   };
