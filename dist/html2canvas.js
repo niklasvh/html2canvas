7cbb4d04b26f0284d3d4d540612d3b675cb73e84
MoyuScript <i@moyu.moe>
2014-09-09 18:42:00 +0300

    Add atob polyfill for ie9 for SVG base64 rendering

--- dist/html2canvas.js
+++ dist/html2canvas.js
@@ -312,7 +312,7 @@ ImageLoader.prototype.addImage = function(images, callback) {
         newImage.args.forEach(function(image) {
             if (!this.imageExists(images, image)) {
                 images.splice(0, 0, callback.call(this, newImage));
-                log('Added image #' + (images.length), image);
+                log('Added image #' + (images.length), typeof(image) === "string" ? image.substring(0, 100) : image);
             }
         }, this);
     };
@@ -1793,7 +1793,7 @@ SVGContainer.prototype.hasFabric = function() {
 };
 
 SVGContainer.prototype.inlineFormatting = function(src) {
-    return (/^data:image\/svg\+xml;base64,/.test(src)) ? window.atob(this.removeContentType(src)) : this.removeContentType(src);
+    return (/^data:image\/svg\+xml;base64,/.test(src)) ? this.decode64(this.removeContentType(src)) : this.removeContentType(src);
 };
 
 SVGContainer.prototype.removeContentType = function(src) {
@@ -1817,6 +1817,45 @@ SVGContainer.prototype.createCanvas = function(resolve) {
         resolve(canvas.lowerCanvasEl);
     };
 };
+
+SVGContainer.prototype.decode64 = function(str) {
+    return (typeof(window.atob) === "function") ? window.atob(str) : decode64(str);
+};
+
+/*
+ * base64-arraybuffer
+ * https://github.com/niklasvh/base64-arraybuffer
+ *
+ * Copyright (c) 2012 Niklas von Hertzen
+ * Licensed under the MIT license.
+ */
+
+function decode64(base64) {
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var len = base64.length, i, encoded1, encoded2, encoded3, encoded4, byte1, byte2, byte3;
+
+    var output = "";
+
+    for (i = 0; i < len; i+=4) {
+        encoded1 = chars.indexOf(base64[i]);
+        encoded2 = chars.indexOf(base64[i+1]);
+        encoded3 = chars.indexOf(base64[i+2]);
+        encoded4 = chars.indexOf(base64[i+3]);
+
+        byte1 = (encoded1 << 2) | (encoded2 >> 4);
+        byte2 = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+        byte3 = ((encoded3 & 3) << 6) | encoded4;
+        if (encoded3 === 64) {
+            output += String.fromCharCode(byte1);
+        } else if (encoded4 === 64 || encoded4 === -1) {
+            output += String.fromCharCode(byte1, byte2);
+        } else{
+            output += String.fromCharCode(byte1, byte2, byte3);
+        }
+    }
+
+    return output;
+}
 
 function SVGNodeContainer(node) {
     this.src = node;
