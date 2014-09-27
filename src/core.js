c083985f93773da63cd5d706849966d20d432dd1
MoyuScript <i@moyu.moe>
2014-09-28 00:11:54 +0300

    Provide fallbacks for html parsing

--- src/core.js
+++ src/core.js
@@ -154,8 +154,22 @@ function loadUrlDocument(src, proxy, document, width, height, options) {
 
 function documentFromHTML(src) {
     return function(html) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, "text/html");
+        var parser = new DOMParser(), doc;
+        try {
+            doc = parser.parseFromString(html, "text/html");
+        } catch(e) {
+            log("DOMParser not supported, falling back to createHTMLDocument");
+            doc = document.implementation.createHTMLDocument("");
+            try {
+                doc.open();
+                doc.write(html);
+                doc.close();
+            } catch(ee) {
+                log("createHTMLDocument write not supported, falling back to document.body.innerHTML");
+                doc.body.innerHTML = html; // ie9 doesnt support writing to documentElement
+            }
+        }
+
         var b = doc.querySelector("base");
         if (!b || !b.href.host) {
             var base = doc.createElement("base");
