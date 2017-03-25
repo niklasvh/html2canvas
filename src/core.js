62b6973b5a4ba7376367bb090424066a374abe9e
Erik Koopmans <erik@erik-koopmans.com>
2017-03-25 17:32:21 -0400

    Fix rendering of offscreen elements

--- src/core.js
+++ src/core.js
@@ -87,8 +87,8 @@ function renderWindow(node, container, options, windowWidth, windowHeight) {
     var support = new Support(clonedWindow.document);
     var imageLoader = new ImageLoader(options, support);
     var bounds = getBounds(node);
-    var width = options.type === "view" ? windowWidth : documentWidth(clonedWindow.document);
-    var height = options.type === "view" ? windowHeight : documentHeight(clonedWindow.document);
+    var width = options.type === "view" ? windowWidth : bounds.right;
+    var height = options.type === "view" ? windowHeight : bounds.bottom;
     var renderer = new options.renderer(width, height, imageLoader, options, document);
     var parser = new NodeParser(node, renderer, support, imageLoader, options);
     return parser.ready.then(function() {
