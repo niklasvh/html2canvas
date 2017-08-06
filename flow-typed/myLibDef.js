f7f445c71ee244f32216e6b8a82085fb6499e146
Niklas von Hertzen <niklasvh@gmail.com>
2017-08-06 18:13:40 +0800

    Add license info to builds (Fix #1126)

--- flow-typed/myLibDef.js
+++ flow-typed/myLibDef.js
@@ -1,1 +1,2 @@
 declare var __DEV__: boolean;
+declare var __VERSION__: string;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')));
+
+const banner =
+`${pkg.title} ${pkg.version} <${pkg.homepage}>
+Copyright (c) ${(new Date()).getFullYear()} ${pkg.author.name} <${pkg.author.url}>
+Released under ${pkg.license} License`;
 
 module.exports = {
     entry: './src/index.js',
@@ -16,7 +25,9 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            '__DEV__': true
-        })
+            '__DEV__': true,
+            '__VERSION__': JSON.stringify(pkg.version)
+        }),
+        new webpack.BannerPlugin(banner)
     ]
 };
