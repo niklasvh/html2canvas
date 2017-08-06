68900c30878baf56429ed5da2c898d43364be93e
Niklas von Hertzen <niklasvh@gmail.com>
2017-08-06 17:37:10 +0800

    Copy CSS properties individual with IE

--- src/Util.js
+++ src/Util.js
@@ -2,3 +2,15 @@
 'use strict';
 
 export const contains = (bit: number, value: number): boolean => (bit & value) !== 0;
+
+export const copyCSSStyles = (style: CSSStyleDeclaration, target: HTMLElement): void => {
+    if (style.cssText) {
+        target.style = style.cssText;
+    } else {
+        // Edge does not provide value for cssText
+        for (let i = style.length - 1; i >= 0; i--) {
+            const property = style.item(i);
+            target.style.setProperty(property, style.getPropertyValue(property));
+        }
+    }
+};
