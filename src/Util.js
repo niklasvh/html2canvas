769d74351c236599260feb1f2dce57f9bfa92107
MoyuScript <i@moyu.moe>
2017-08-13 21:39:57 +0800

    Fix recursion for safari 10 on pseudoelements

--- src/Util.js
+++ src/Util.js
@@ -4,13 +4,15 @@
 export const contains = (bit: number, value: number): boolean => (bit & value) !== 0;
 
 export const copyCSSStyles = (style: CSSStyleDeclaration, target: HTMLElement): void => {
-    if (style.cssText) {
-        target.style.cssText = style.cssText;
-    } else {
-        // Edge does not provide value for cssText
-        for (let i = style.length - 1; i >= 0; i--) {
-            const property = style.item(i);
+    // Edge does not provide value for cssText
+    for (let i = style.length - 1; i >= 0; i--) {
+        const property = style.item(i);
+        // Safari shows pseudoelements if content is set
+        if (property !== 'content') {
             target.style.setProperty(property, style.getPropertyValue(property));
         }
     }
 };
+
+export const SMALL_IMAGE =
+    'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
