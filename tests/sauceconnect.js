1e854b5d044d2dc5dd8732c2d8bb41a9aea4e52e
MoyuScript <i@moyu.moe>
2017-08-13 23:24:39 +0800

    Add sauceconnect launcher

---
+++ tests/sauceconnect.js
@@ -0,0 +1,16 @@
+const sauceConnectLauncher = require('sauce-connect-launcher');
+
+sauceConnectLauncher(
+    {
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        logger: console.log
+    },
+    err => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.log('Sauce Connect ready');
+    }
+);
