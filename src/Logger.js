90a8422938a42fd262238086643da636a01e8e0e
Niklas von Hertzen <niklasvh@gmail.com>
2017-09-11 22:36:23 +0800

    Implement Iframe rendering

--- src/Logger.js
+++ src/Logger.js
@@ -3,9 +3,15 @@
 
 export default class Logger {
     start: number;
+    id: ?string;
 
-    constructor() {
-        this.start = Date.now();
+    constructor(id: ?string, start: ?number) {
+        this.start = start ? start : Date.now();
+        this.id = id;
+    }
+
+    child(id: string) {
+        return new Logger(id, this.start);
     }
 
     // eslint-disable-next-line flowtype/no-weak-types
@@ -15,7 +21,10 @@ export default class Logger {
                 .call(window.console.log, window.console)
                 .apply(
                     window.console,
-                    [Date.now() - this.start + 'ms', 'html2canvas:'].concat([].slice.call(args, 0))
+                    [
+                        Date.now() - this.start + 'ms',
+                        this.id ? `html2canvas (${this.id}):` : 'html2canvas:'
+                    ].concat([].slice.call(args, 0))
                 );
         }
     }
@@ -27,7 +36,10 @@ export default class Logger {
                 .call(window.console.error, window.console)
                 .apply(
                     window.console,
-                    [Date.now() - this.start + 'ms', 'html2canvas:'].concat([].slice.call(args, 0))
+                    [
+                        Date.now() - this.start + 'ms',
+                        this.id ? `html2canvas (${this.id}):` : 'html2canvas:'
+                    ].concat([].slice.call(args, 0))
                 );
         }
     }
