88d50b107b87c946587503660de731079ed3b32e
MoyuScript <i@moyu.moe>
2017-12-03 17:07:10 +0800

    Add npm and minified builds

--- tests/node/package.js
+++ tests/node/package.js
@@ -1,24 +1,15 @@
-var assert = require('assert');
-var path = require('path');
-var html2canvas = require('../../');
+const assert = require('assert');
+const html2canvas = require('../../');
 
-describe('Package', function() {
-    it('should have html2canvas defined', function() {
+describe('Package', () => {
+    it('should have html2canvas defined', () => {
         assert.equal(typeof html2canvas, 'function');
     });
-});
 
-describe.only('requirejs', function() {
-    var requirejs = require('requirejs');
-
-    requirejs.config({
-        baseUrl: path.resolve(__dirname, '../../dist')
-    });
-
-    it('should have html2canvas defined', function(done) {
-        requirejs(['html2canvas'], function(h2c) {
-            assert.equal(typeof h2c, 'function');
+    it('should have html2canvas defined', done => {
+        html2canvas('').catch((err) => {
+            assert.equal(err, 'Provided element is not within a Document');
             done();
-        });
+        })
     });
 });
