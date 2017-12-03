0e8a924ea24c8bc962a5a0360c684d1a759968ff
Niklas von Hertzen <niklasvh@gmail.com>
2017-12-03 17:30:52 +0800

    Fix formatting

--- tests/node/package.js
+++ tests/node/package.js
@@ -7,9 +7,9 @@ describe('Package', () => {
     });
 
     it('should have html2canvas defined', done => {
-        html2canvas('').catch((err) => {
+        html2canvas('').catch(err => {
             assert.equal(err, 'Provided element is not within a Document');
             done();
-        })
+        });
     });
 });
