86a8da1a23ca8231bf363d92cf814e1b5ffd3302
MoyuScript <i@moyu.moe>
2018-01-08 22:21:11 +0800

    Update website styles

--- www/src/components/footer.js
+++ www/src/components/footer.js
@@ -4,14 +4,25 @@ export default () =>
     <footer
         css={{
             backgroundColor: '#558b2f',
-            color: 'rgba(255,255,255,0.8)',
+            color: 'rgba(255,255,255, 0.8)',
             fontWeight: 300,
             minHeight: '50px',
             lineHeight: '50px',
             padding: '10px 0px'
         }}
     >
-        <div css={{margin: '0 auto', width: '85%'}}>
+        <div
+            css={{
+                margin: '0 auto',
+                fontSize: '10.5px',
+                textAlign: 'center',
+                '@media(min-width: 1000px)': {
+                    textAlign: 'left',
+                    width: '85%',
+                    fontSize: '14.5px'
+                }
+            }}
+        >
             Created by{' '}
             <a href="https://hertzen.com" css={{color: '#fff', fontWeight: 'bold'}}>
                 Niklas von Hertzen
