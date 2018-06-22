da461d74dfe899fcfe870af27729574ee9207973
Bishares <38129103+Bishares@users.noreply.github.com>
2018-06-22 10:15:20 +0200

    Update flex.js
    added justify-content

--- src/parsing/flex.js
+++ src/parsing/flex.js
@@ -21,3 +21,30 @@ export const parseFlexWrap = (flexWrap: string): FlexWrap => {
             return FLEX_WRAP.NOWRAP;
     }
 };
+
+export const JUSTIFY_CONTENT = {
+    FLEX_START: 0,
+    FLEX_END: 1,
+    CENTER: 2,
+    SPACE_BETWEEN: 3,
+    SPACE_AROUND: 4
+};
+
+export type JustifyContent = $Values<typeof JUSTIFY_CONTENT>;
+
+export const parseJustifyContent = (justifyContent: string): JustifyContent => {
+    switch (justifyContent) {
+        case 'flex-start':
+            return JUSTIFY_CONTENT.FLEX_START;
+        case 'flex-end':
+            return JUSTIFY_CONTENT.FLEX_END;
+        case 'center':
+            return JUSTIFY_CONTENT.CENTER;
+        case 'space-between':
+            return JUSTIFY_CONTENT.SPACE_BETWEEN;
+        case 'space-around':
+            return JUSTIFY_CONTENT.SPACE_AROUND;
+        default:
+            return JUSTIFY_CONTENT.FLEX_START;
+    }
+};
