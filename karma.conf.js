8a1cbfbc8a6ee5c65290cdabaea461f65f96a42a
Niklas von Hertzen <niklasvh@gmail.com>
2019-04-06 16:41:28 -0700

    Publish junit reports

--- karma.conf.js
+++ karma.conf.js
@@ -136,7 +136,11 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'saucelabs'],
+        reporters: ['dots', 'junit'],
+
+        junitReporter: {
+            outputDir: 'tmp/junit/'
+        },
 
         // web server port
         port,
