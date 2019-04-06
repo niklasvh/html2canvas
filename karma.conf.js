4004069bf2b4aef6dde73c930abe0e99b9302da2
Niklas von Hertzen <niklasvh@gmail.com>
2019-04-06 12:42:37 -0700

    Update to dots reporter

--- karma.conf.js
+++ karma.conf.js
@@ -122,7 +122,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'saucelabs'],
+        reporters: ['dots', 'saucelabs'],
 
         // web server port
         port,
