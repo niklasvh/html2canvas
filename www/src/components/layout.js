24823d04919239ec7b41fe853f108eb7bc707d52
Niklas von Hertzen <niklasvh@gmail.com>
2019-04-05 21:28:45 -0700

    Upgrade gatsbyjs to v2

--- www/src/components/layout.js
+++ www/src/components/layout.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-require('prismjs/themes/prism-solarizedlight.css');
-import './index.css';
+import './layout.css';
 import Example from '../components/example';
 
+require('prismjs/themes/prism-solarizedlight.css');
 const TemplateWrapper = ({children}) =>
     <div>
         <Helmet title="html2canvas - Screenshots with JavaScript" />
-        {children()}
+        {children}
         <Example />
     </div>;
-
-TemplateWrapper.propTypes = {
-    children: PropTypes.func
-};
 
 export default TemplateWrapper;
