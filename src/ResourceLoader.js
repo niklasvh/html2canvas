ada00dac69246d6a1421e751ff4c0648f195f7c2
Niklas von Hertzen <niklasvh@gmail.com>
2019-04-09 19:59:34 -0700

    debug

--- src/ResourceLoader.js
+++ src/ResourceLoader.js
@@ -134,7 +134,7 @@ export default class ResourceLoader {
                 const img = new Image();
                 img.onload = () => resolve(img);
                 //ios safari 10.3 taints canvas with data urls unless crossOrigin is set to anonymous
-                if (useCORS) {
+                if (!isInlineBase64Image(src) && useCORS) {
                     img.crossOrigin = 'anonymous';
                 }
 
