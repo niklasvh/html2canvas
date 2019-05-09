ec6438176be17ce31c04f18e83c03df14fe2e9b8
Niklas von Hertzen <niklasvh@gmail.com>
2019-05-08 22:28:18 -0700

    fix: ie logging

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,19 +1,29 @@
 export class Logger {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static debug(...args: any) {
-        // eslint-disable-next-line no-console
-        console.debug(...args);
+        if (typeof console.debug === 'function') {
+            // eslint-disable-next-line no-console
+            console.debug(...args);
+        } else {
+            Logger.info(...args);
+        }
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static info(...args: any) {
-        // eslint-disable-next-line no-console
-        console.info(...args);
+        if (typeof console.info === 'function') {
+            // eslint-disable-next-line no-console
+            console.info(...args);
+        }
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static error(...args: any) {
-        // eslint-disable-next-line no-console
-        console.error(...args);
+        if (typeof console.error === 'function') {
+            // eslint-disable-next-line no-console
+            console.error(...args);
+        } else {
+            Logger.info(...args);
+        }
     }
 }
