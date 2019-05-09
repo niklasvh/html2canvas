b403d98cdf1dbf327d5ea415f7b6470dfbabee0a
Niklas von Hertzen <niklasvh@gmail.com>
2019-05-08 22:37:11 -0700

    fix: check for console availability

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -2,7 +2,7 @@ export class Logger {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static debug(...args: any) {
         // eslint-disable-next-line no-console
-        if (typeof console.debug === 'function') {
+        if (window.console && typeof console.debug === 'function') {
             // eslint-disable-next-line no-console
             console.debug(...args);
         } else {
@@ -13,7 +13,7 @@ export class Logger {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static info(...args: any) {
         // eslint-disable-next-line no-console
-        if (typeof console.info === 'function') {
+        if (window.console && typeof console.info === 'function') {
             // eslint-disable-next-line no-console
             console.info(...args);
         }
@@ -22,7 +22,7 @@ export class Logger {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static error(...args: any) {
         // eslint-disable-next-line no-console
-        if (typeof console.error === 'function') {
+        if (window.console && typeof console.error === 'function') {
             // eslint-disable-next-line no-console
             console.error(...args);
         } else {
