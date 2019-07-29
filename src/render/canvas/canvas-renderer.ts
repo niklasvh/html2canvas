3dcbf99f947651e63f3bf5e0185007b01ea2aa3b
Graham Kane <grahamkane@gmail.com>
2019-07-29 16:25:17 +0100

    support font-family names with numbers in (e.g. Font Awesome 5)

--- src/render/canvas/canvas-renderer.ts
+++ src/render/canvas/canvas-renderer.ts
@@ -155,7 +155,7 @@ export class CanvasRenderer {
         const fontVariant = styles.fontVariant
             .filter(variant => variant === 'normal' || variant === 'small-caps')
             .join('');
-        const fontFamily = styles.fontFamily.join(', ');
+        const fontFamily = styles.fontFamily.map(fontFamily => `"${fontFamily}"`).join(', ');
         const fontSize = isDimensionToken(styles.fontSize)
             ? `${styles.fontSize.number}${styles.fontSize.unit}`
             : `${styles.fontSize.number}px`;
