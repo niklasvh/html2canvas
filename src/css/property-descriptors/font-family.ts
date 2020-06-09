c2177725e74c519671133fa1d6465e87aa47996f
James Addison <jay@jp-hosting.net>
2020-06-09 16:43:43 +0100

    Refactor: use switch statement in preference to multiple if-conditions

--- src/css/property-descriptors/font-family.ts
+++ src/css/property-descriptors/font-family.ts
@@ -1,5 +1,5 @@
 import {IPropertyListDescriptor, PropertyDescriptorParsingType} from '../IPropertyDescriptor';
-import {CSSValue, isIdentToken, isNumberToken, isStringToken} from '../syntax/parser';
+import {CSSValue} from '../syntax/parser';
 import {TokenType} from '../syntax/tokenizer';
 
 export type FONT_FAMILY = string;
@@ -15,15 +15,18 @@ export const fontFamily: IPropertyListDescriptor<FontFamily> = {
         const accumulator: string[] = [];
         const results: string[] = [];
         tokens.forEach(token => {
-            if (isIdentToken(token) || isStringToken(token)) {
-                accumulator.push(token.value);
-            }
-            if (isNumberToken(token)) {
-                accumulator.push(token.number.toString());
-            }
-            if (token.type === TokenType.COMMA_TOKEN) {
-                results.push(`'${accumulator.join(' ')}'`);
-                accumulator.length = 0;
+            switch (token.type) {
+                case TokenType.IDENT_TOKEN:
+                case TokenType.STRING_TOKEN:
+                    accumulator.push(token.value);
+                    break;
+                case TokenType.NUMBER_TOKEN:
+                    accumulator.push(token.number.toString());
+                    break;
+                case TokenType.COMMA_TOKEN:
+                    results.push(`'${accumulator.join(' ')}'`);
+                    accumulator.length = 0;
+                    break;
             }
         });
         if (accumulator.length) {
