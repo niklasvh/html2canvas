788c59df43cbe0f9de54daee70ab0596ad639b72
Stefan Niederhauser <ghuder5@gmx.ch>
2020-07-22 16:57:15 +0200

    fix details

--- src/css/types/duration.ts
+++ src/css/types/duration.ts
@@ -6,9 +6,12 @@ export const duration: ITypeDescriptor<number> = {
     name: 'duration',
     parse: (value: CSSValue): number => {
         if (value.type === TokenType.DIMENSION_TOKEN) {
-            return (value.unit.toLowerCase() === 's' ? 1000 : 1) * value.number;
+            switch (value.unit.toLowerCase()){
+                case 's': return 1000*value.number;
+                case 'ms': return value.number;
+            }
         }
 
-        throw new Error(`Unsupported angle type`);
+        throw new Error(`Unsupported duration type`);
     }
 }
