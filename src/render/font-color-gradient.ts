0dcb10ce7c1fb2b23ec104452ccd560b1744cff5
ysk2014 <1181102772@qq.com>
2020-10-14 16:32:36 +0800

    Modify code writing

--- src/render/font-color-gradient.ts
+++ src/render/font-color-gradient.ts
@@ -3,16 +3,11 @@ import {isLinearGradient, isRadialGradient} from '../css/types/image';
 import {BACKGROUND_CLIP} from '../css/property-descriptors/background-clip';
 
 export const isFontColorGradient = (styles: CSSParsedDeclaration) => {
-    if (
+    return (
         styles.backgroundImage.length === 1 &&
-        (isLinearGradient(styles.backgroundImage[0]) || isRadialGradient(styles.backgroundImage[0]))
-    ) {
-        return (
-            (styles.textFillColor === 0 || styles.color === 0) &&
-            styles.backgroundClip.length === 1 &&
-            styles.backgroundClip[0] === BACKGROUND_CLIP.TEXT
-        );
-    } else {
-        return false;
-    }
+        (isLinearGradient(styles.backgroundImage[0]) || isRadialGradient(styles.backgroundImage[0])) &&
+        (styles.textFillColor === 0 || styles.color === 0) &&
+        styles.backgroundClip.length === 1 &&
+        styles.backgroundClip[0] === BACKGROUND_CLIP.TEXT
+    );
 };
