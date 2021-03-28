8c0a2394690ca7136d459ef2f295bbc87649f66e
hocine BENKHADRA <miniben90@hotmail.com>
2021-03-28 11:07:26 +0200

    fix(svg-element-container): fix issue with dynamic svg.

--- src/dom/replaced-elements/svg-element-container.ts
+++ src/dom/replaced-elements/svg-element-container.ts
@@ -8,6 +8,29 @@ export class SVGElementContainer extends ElementContainer {
 
     constructor(img: SVGSVGElement) {
         super(img);
+        const percentValueTest: RegExp = /^[0-9]{1,3}\%$/i;
+
+        const widthAttribute: string = img.getAttribute('width') || '';
+        const widthStyle: string = img.style.width || '';
+        const heightAttribute: string = img.getAttribute('width') || '';
+        const heightStyle: string = img.style.width || '';
+
+        if (
+            percentValueTest.test(widthAttribute) ||
+            percentValueTest.test(widthStyle) ||
+            percentValueTest.test(heightAttribute) ||
+            percentValueTest.test(heightStyle)
+        ) {
+            if (!img.clientWidth && !img.clientHeight && img.parentElement) {
+                const parentStyle: CSSStyleDeclaration = window.getComputedStyle(img.parentElement);
+                img.setAttribute('width', parentStyle.width || '100%');
+                img.setAttribute('height', parentStyle.height || '100%');
+            } else {
+                img.setAttribute('width', img.clientWidth.toString());
+                img.setAttribute('height', img.clientHeight.toString());
+            }
+        }
+
         const s = new XMLSerializer();
         this.svg = `data:image/svg+xml,${encodeURIComponent(s.serializeToString(img))}`;
         this.intrinsicWidth = img.width.baseVal.value;
