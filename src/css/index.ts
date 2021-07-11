44296e529368140ec06a937383e05f3074b19ee2
Niklas von Hertzen <niklasvh@gmail.com>
2021-07-11 22:11:29 +0800

    fix: text-decoration-line fallback (#2088) (#2567)

--- src/css/index.ts
+++ src/css/index.ts
@@ -191,8 +191,11 @@ export class CSSParsedDeclaration {
         this.paddingLeft = parse(paddingLeft, declaration.paddingLeft);
         this.position = parse(position, declaration.position);
         this.textAlign = parse(textAlign, declaration.textAlign);
-        this.textDecorationColor = parse(textDecorationColor, declaration.textDecorationColor || declaration.color);
-        this.textDecorationLine = parse(textDecorationLine, declaration.textDecorationLine);
+        this.textDecorationColor = parse(textDecorationColor, declaration.textDecorationColor ?? declaration.color);
+        this.textDecorationLine = parse(
+            textDecorationLine,
+            declaration.textDecorationLine ?? declaration.textDecoration
+        );
         this.textShadow = parse(textShadow, declaration.textShadow);
         this.textTransform = parse(textTransform, declaration.textTransform);
         this.transform = parse(transform, declaration.transform);
