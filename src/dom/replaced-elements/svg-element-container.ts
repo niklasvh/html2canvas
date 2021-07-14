d7d64e1a50ef6c4964eec78ee8bf9200daf2e550
Niklas von Hertzen <niklasvh@gmail.com>
2021-07-14 20:19:03 +0800

    fix: responsive svg images

--- src/dom/replaced-elements/svg-element-container.ts
+++ src/dom/replaced-elements/svg-element-container.ts
@@ -1,5 +1,6 @@
 import {ElementContainer} from '../element-container';
 import {CacheStorage} from '../../core/cache-storage';
+import {parseBounds} from '../../css/layout/bounds';
 
 export class SVGElementContainer extends ElementContainer {
     svg: string;
@@ -9,6 +10,10 @@ export class SVGElementContainer extends ElementContainer {
     constructor(img: SVGSVGElement) {
         super(img);
         const s = new XMLSerializer();
+        const bounds = parseBounds(img);
+        img.setAttribute('width', `${bounds.width}px`);
+        img.setAttribute('height', `${bounds.height}px`);
+
         this.svg = `data:image/svg+xml,${encodeURIComponent(s.serializeToString(img))}`;
         this.intrinsicWidth = img.width.baseVal.value;
         this.intrinsicHeight = img.height.baseVal.value;
